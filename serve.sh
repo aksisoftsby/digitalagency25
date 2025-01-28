521e3d92da58b519ad12c8a11bf2edd0e5f07436
root <root@server2gb>
2025-01-28 01:21:58 +0000

    Update Current date: Tue Jan 28 01:21:58 UTC 2025

--- serve.sh
+++ serve.sh
@@ -1,3 +1,3 @@
 # akses ke http://103.37.125.10:4000
 # bundle exec jekyll serve --host 103.37.125.10
-bundle exec jekyll serve --host 0.0.0.0
+JEKYLL_ENV=production bundle exec jekyll serve --host 0.0.0.0
